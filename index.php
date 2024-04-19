b078f1e2bc0ae76108981b1f4c2d045ba2175e37
Johan  Antonio <147890607+johansitoweb@users.noreply.github.com>
2024-04-20 01:51:08 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,259 @@
+<?php
+$nodrm="Lesplitis";
+$fggf="Home";
+$gngh="About";
+$fjdh="mas";
+$contacto="contacto";
+//echo "hola soy php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="rifufufdi">
+    <style>
+        .rifufufdi{
+            background-image: url("hola.png");
+        }
+    </style>
+    <!--
+        ***************************************************************
+        *                                                             *
+        *                                                             *
+        * Estare creando una pagina web con php html css y tailwindcss*
+        *                                                             *
+        *                                                             *
+        *                                                             *
+        ***************************************************************
+    -->
+    <header class="text-gray-600 body-font">
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
+        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+      </svg>
+      <span class="ml-3 text-xl">
+        <font style="vertical-align: inherit;">
+          <font style="vertical-align: inherit;"><?php echo$nodrm?></font>
+        </font>
+      </span>
+    </a>
+    <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+      <a class="mr-5 hover:text-gray-900">
+        <font style="vertical-align: inherit;">
+          <font style="vertical-align: inherit;"><?php echo$fggf?></font>
+        </font>
+      </a>
+      <a class="mr-5 hover:text-gray-900">
+        <font style="vertical-align: inherit;">
+          <font style="vertical-align: inherit;"><?php echo$gngh?></font>
+        </font>
+      </a>
+      <a class="mr-5 hover:text-gray-900">
+        <font style="vertical-align: inherit;">
+          <font style="vertical-align: inherit;"><?php echo $fjdh?></font>
+        </font>
+      </a>
+      <a class="mr-5 hover:text-gray-900">
+        <font style="vertical-align: inherit;">
+          <font style="vertical-align: inherit;"><?php echo $contacto?></font>
+        </font>
+      </a>
+    </nav>
+    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+      <font style="vertical-align: inherit;">
+        <font style="vertical-align: inherit;">Bievenido/as</font>
+      </font>
+      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+    </button>
+  </div>
+</header>
+ <!--
+        ***************************************************************
+        *                                                             *
+        *                                                             *
+        * Estare creando una pagina web con php html css y tailwindcss*
+        *                                                             *
+        *                                                             *
+        *                                                             *
+        ***************************************************************
+    -->
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <h1 class="sm:text-3xl text-2xl font-medium title-font text-center text-gray-900 mb-20">
+      <font style="vertical-align: inherit;">
+        <font style="vertical-align: inherit;">Vaqueros crudos Heirloom Man Braid </font>
+      </font>
+      <br class="hidden sm:block">
+      <font style="vertical-align: inherit;">
+        <font style="vertical-align: inherit;">Selfies Wayfarers</font>
+      </font>
+    </h1>
+    <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+      <div class="p-4 md:w-1/3 flex">
+        <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-4 flex-shrink-0">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+            <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+          </svg>
+        </div>
+        <div class="flex-grow pl-6">
+          <h2 class="text-gray-900 text-lg title-font font-medium mb-2">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">Estrellas fugaces</font>
+            </font>
+          </h2>
+          <p class="leading-relaxed text-base">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">Botella azul crucifijo vinilo post-irónico tostada vegana de cuatro dólares taxidermia. Gastropub indxgo jugo poutine, rampas microdosificación banh mi pug VHS esforzarse ugh islandia kickstarter tumblr live-edge tilde.</font>
+            </font>
+          </p>
+          <a class="mt-3 text-green-500 inline-flex items-center">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">Aprende más</font>
+            </font>
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3 flex">
+        <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-4 flex-shrink-0">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+            <circle cx="6" cy="6" r="3"></circle>
+            <circle cx="6" cy="18" r="3"></circle>
+            <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+          </svg>
+        </div>
+        <div class="flex-grow pl-6">
+          <h2 class="text-gray-900 text-lg title-font font-medium mb-2">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">El catalizador</font>
+            </font>
+          </h2>
+          <p class="leading-relaxed text-base">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">Botella azul crucifijo vinilo post-irónico tostada vegana de cuatro dólares taxidermia. Gastropub indxgo jugo poutine, rampas microdosificación banh mi pug VHS esforzarse ugh islandia kickstarter tumblr live-edge tilde.</font>
+            </font>
+          </p>
+          <a class="mt-3 text-green-500 inline-flex items-center">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">Aprende más</font>
+            </font>
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3 flex">
+        <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-4 flex-shrink-0">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+            <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+            <circle cx="12" cy="7" r="4"></circle>
+          </svg>
+        </div>
+        <div class="flex-grow pl-6">
+          <h2 class="text-gray-900 text-lg title-font font-medium mb-2">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">Neptuno</font>
+            </font>
+          </h2>
+          <p class="leading-relaxed text-base">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">Botella azul crucifijo vinilo post-irónico tostada vegana de cuatro dólares taxidermia. Gastropub indxgo jugo poutine, rampas microdosificación banh mi pug VHS esforzarse ugh islandia kickstarter tumblr live-edge tilde.</font>
+            </font>
+          </p>
+          <a class="mt-3 text-green-500 inline-flex items-center">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">Aprende más</font>
+            </font>
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    <!--
+        ***************************************************************
+        *                                                             *
+        *                                                             *
+        * Estare creando una pagina web con php html css y tailwindcss*
+        *                                                             *
+        *                                                             *
+        *                                                             *
+        ***************************************************************
+    -->
+    <section class="text-gray-600 body-font relative">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-12">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Registrate.</p>
+    </div>
+    <div class="lg:w-1/2 md:w-2/3 mx-auto">
+      <div class="flex flex-wrap -m-2">
+        <div class="p-2 w-1/2">
+          <div class="relative">
+            <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+            <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+          </div>
+        </div>
+        <div class="p-2 w-1/2">
+          <div class="relative">
+            <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+            <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+          </div>
+        </div>
+        <div class="p-2 w-full">
+          <div class="relative">
+            <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+            <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+          </div>
+        </div>
+        <div class="p-2 w-full">
+          <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Enviar</button>
+        </div>
+        <div class="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+          <a class="text-indigo-500">example@email.com</a>
+          <p class="leading-normal my-5">49 Smith St.
+            <br>Saint Cloud, MN 56301
+          </p>
+          <span class="inline-flex">
+            <a class="text-gray-500">
+              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+              </svg>
+            </a>
+            <a class="ml-4 text-gray-500">
+              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+              </svg>
+            </a>
+            <a class="ml-4 text-gray-500">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+              </svg>
+            </a>
+            <a class="ml-4 text-gray-500">
+              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+              </svg>
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</body>
+</html>
